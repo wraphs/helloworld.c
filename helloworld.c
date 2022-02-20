3d351d3fe32f105b2ba80bf2a4a4ccbd6e18cbc3
Pedro Crispim Pereira <57925151+wraphs@users.noreply.github.com>
2022-02-19 23:33:02 -0300

    Add files via upload

---
+++ helloworld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (){
+
+    printf("and you like concorde\n");
+    printf("i came a gentle hill racer\n");
+    printf("i was breathless upon every mountain\n");
+    printf("just to look for your light");
+    return 0;
+}
